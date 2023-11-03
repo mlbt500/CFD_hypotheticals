89c140df7879ede3e38f200e3f0175d833d9fda8
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-11-03 10:02:00 +0000

    I still need to double the results for the hypothetical strike price.

--- Solar and £65 hyp.R
+++ Solar and £65 hyp.R
@@ -6,12 +6,42 @@ library(lubridate)
 # Fetch data from the URL
 url <- "https://www.lowcarboncontracts.uk/data-portal/dataset/8a82caf8-3ec2-45a2-b1bd-581411d61cde/resource/ab7ea978-2f03-4ee4-99ef-f9cdf398b849/download/actual-cfd-generation-and-avoided-ghg-emissions.csv"
 data <- read_csv(url)
-
+cpi <- read.csv("series-181023.csv")
+cpi_filtered <- cpi[-c(1:527),]
 # Filter data for the specific CfD ID
 solar83 <- data[data$CfD_ID == "AAA-LIG-176", ]
+solar83 <-solar83[-1,]
 
-# Calculate the hypothetical CFD Payments for £65 Strike Price
-solar83$CFD_Payments_65 <- (65 - solar83$Market_Reference_Price_GBP_Per_MWh) * solar83$CFD_Generation_MWh
+# Ensure CPI values are numeric if they aren't already
+cpi_filtered$CPI.INDEX.00..ALL.ITEMS.2015.100 <- as.numeric(cpi_filtered$CPI.INDEX.00..ALL.ITEMS.2015.100)
+
+# Create a lookup vector for CPI values with the Title as names
+cpi_lookup <- setNames(cpi_filtered$CPI.INDEX.00..ALL.ITEMS.2015.100, cpi_filtered$Title)
+
+# Format the Settlement_Date column to match the CPI Title format
+solar83$Month_Year <- format(solar83$Settlement_Date, "%Y %b")
+
+# Check for any Month_Year in solar83 that doesn't have a match in cpi_lookup
+non_matching <- setdiff(solar83$Month_Year, names(cpi_lookup))
+
+# Print non-matching Month_Year values if there are any
+if(length(non_matching) > 0) {
+  print("Non-matching Month_Year values:")
+  print(non_matching)
+}
+
+# Match CPI values to the solar83 dataset using Month_Year
+solar83$CPI_for_date <- cpi_lookup[solar83$Month_Year]
+
+# Calculate the adjustment factor using CPI for July 2016 as the base
+base_cpi <- cpi_lookup["2016 JUL"]
+solar83$adjustment_factor <- ifelse(is.na(solar83$CPI_for_date), NA, base_cpi / solar83$CPI_for_date)
+
+# Apply the adjustment factor to the CFD_Payments_GBP column
+solar83$Adjusted_CFD_Payments_GBP <- ifelse(is.na(solar83$adjustment_factor), NA, solar83$CFD_Payments_GBP * solar83$adjustment_factor)
+
+# Create the new tibble solar83a with the adjusted payments
+solar83a <- solar83
 
 # Define the pandemic start date
 pandemic_start_date <- as.Date("2020-03-01")
@@ -20,50 +50,94 @@ pandemic_start_date <- as.Date("2020-03-01")
 # Extract the date portion of the column
 solar83$DateOnly <- as.Date(solar83$Settlement_Date)
 
+# Assuming 'pandemic_start_date' and 'solar83a' are already defined
+
 # Filter the data frame based on the date condition
-pre_pandemic_data <- filter(solar83, DateOnly < pandemic_start_date)
+# Make sure to replace 'DateOnly' with the actual date column name in 'solar83a'
+pre_pandemic_data <- filter(solar83a, Settlement_Date < pandemic_start_date)
 
-# Remove the temporary "DateOnly" column if not needed
-solar83 <- select(solar83, -DateOnly)
+# Divide through by 2012 strike price
+names <- c("CFD_Payments_GBP", "Strike_Price_GBP_Per_MWh", "Market_Reference_Price_GBP_Per_MWh")
+solar2a <- solar83a
+for(name in names){
+  solar2a[[name]] <- solar83a[[name]] / 79.23
+}
 
-# Compute the total savings/costs for both scenarios before the pandemic
-pre_pandemic_data <- filter(solar83, Settlement_Date < pandemic_start_date)
-actual_total_payment_pre_pandemic <- sum(pre_pandemic_data$CFD_Payments_GBP, na.rm = TRUE)
-hypothetical_65_total_payment_pre_pandemic <- sum(pre_pandemic_data$CFD_Payments_65, na.rm = TRUE)
+# solar with a strike price of 65
+solar65a <- solar2a
+for(name in names){
+  solar65a[[name]] <- solar2a[[name]] * 65
+}
 
-# Create the footnote text
-actual_footnote <- paste("* Total over the contract's lifetime:", 
-                         ifelse(actual_total_payment < 0, 
-                                paste("\nSaved consumers", round(abs(actual_total_payment), 2), "GBP"), 
-                                paste("\nCost consumers", round(actual_total_payment, 2), "GBP")),
-                         "\n** From contract start to pre-pandemic:", 
-                         ifelse(actual_total_payment_pre_pandemic < 0, 
-                                paste("\nSaved consumers", round(abs(actual_total_payment_pre_pandemic), 2), "GBP"), 
-                                paste("\nCost consumers", round(actual_total_payment_pre_pandemic, 2), "GBP")))
+# solar with a strike price of 75
+solar75a <- solar2a
+for(name in names){
+  solar75a[[name]] <- solar2a[[name]] * 75
+}
 
-hypothetical_65_footnote <- paste("* Total over the contract's lifetime:", 
-                                  ifelse(hypothetical_65_total_payment < 0, 
-                                         paste("\nWould have saved consumers", round(abs(hypothetical_65_total_payment), 2), "GBP"), 
-                                         paste("\nWould have cost consumers", round(hypothetical_65_total_payment, 2), "GBP")),
-                                  "\n** From contract start to pre-pandemic:", 
-                                  ifelse(hypothetical_65_total_payment_pre_pandemic < 0, 
-                                         paste("\nWould have saved consumers", round(abs(hypothetical_65_total_payment_pre_pandemic), 2), "GBP"), 
-                                         paste("\nWould have cost consumers", round(hypothetical_65_total_payment_pre_pandemic, 2), "GBP")))
+# Define the discount rate
+annual_discount_rate <- 0.035
 
-# Set up the layout to have one row and two columns
-par(mfrow = c(1, 2))
+# Define the base date for discounting (July 2016)
+base_date <- as.Date("2016-07-01")
 
-# Increase the bottom margin using mar parameter
-par(mar = c(12, 4, 4, 2) + 0.1)
+# Function to calculate the present value of a payment
+calculate_pv <- function(payment, date, base_date, rate) {
+  # Calculate the time in years between the payment date and the base date
+  time_in_years <- as.numeric(difftime(date, base_date, units = "days")) / 365.25
+  
+  # Calculate the present value
+  pv <- payment / ((1 + rate) ** time_in_years)
+  
+  return(pv)
+}
 
+# Sum of discounted payments for each data frame
+sum_pv_solar65a <- sum(sapply(1:nrow(solar65a), function(i) calculate_pv(solar65a$CFD_Payments_GBP[i], solar65a$Settlement_Date[i], base_date, annual_discount_rate)))
+sum_pv_solar75a <- sum(sapply(1:nrow(solar75a), function(i) calculate_pv(solar75a$CFD_Payments_GBP[i], solar75a$Settlement_Date[i], base_date, annual_discount_rate)))
+sum_pv_solar83a <- sum(sapply(1:nrow(solar83a), function(i) calculate_pv(solar83a$CFD_Payments_GBP[i], solar83a$Settlement_Date[i], base_date, annual_discount_rate)))
 
-plot(solar83$Settlement_Date, solar83$CFD_Payments_GBP, type="l", 
-     xlab="Date", ylab="CFD Payments (GBP)", main="CFD Payments for £83 Solar PV Over Time")
-abline(h=0, col="red", lwd=1.5)
-mtext(side=1, line=8, adj=0, actual_footnote)
+# Output the results
+sum_pv_solar65a
+sum_pv_solar75a
+sum_pv_solar83a
 
-# Hypothetical CFD Payments for £65
-plot(solar83$Settlement_Date, solar83$CFD_Payments_65, type="l", 
-     xlab="Date", ylab="CFD Payments (GBP)", main="Hypothetical CFD Payments for £65 Strike Price")
-abline(h=0, col="red", lwd=1.5)
-mtext(side=1, line=8, adj=0, hypothetical_65_footnote) 
+# Function to calculate the total discounted payment for a given period
+calculate_discounted_total <- function(data_frame, start_date, end_date, discount_rate, base_date) {
+  # Filter the data for the given period
+  period_data <- subset(data_frame, Settlement_Date >= start_date & Settlement_Date < end_date)
+  
+  # Calculate the present value of each payment
+  pv_payments <- sapply(1:nrow(period_data), function(i) {
+    calculate_pv(period_data$CFD_Payments_GBP[i], period_data$Settlement_Date[i], base_date, discount_rate)
+  })
+  
+  # Sum the present values
+  total_discounted_payment <- sum(pv_payments, na.rm = TRUE)
+  
+  return(total_discounted_payment)
+}
+
+# Calculate the total discounted payments before and after the pandemic for each strike rate
+total_before_solar65a <- calculate_discounted_total(solar65a, base_date, pandemic_start_date, annual_discount_rate, base_date)
+total_after_solar65a <- calculate_discounted_total(solar65a, pandemic_start_date, Sys.Date(), annual_discount_rate, base_date)
+
+total_before_solar75a <- calculate_discounted_total(solar75a, base_date, pandemic_start_date, annual_discount_rate, base_date)
+total_after_solar75a <- calculate_discounted_total(solar75a, pandemic_start_date, Sys.Date(), annual_discount_rate, base_date)
+
+total_before_solar83a <- calculate_discounted_total(solar83a, base_date, pandemic_start_date, annual_discount_rate, base_date)
+total_after_solar83a <- calculate_discounted_total(solar83a, pandemic_start_date, Sys.Date(), annual_discount_rate, base_date)
+
+# Output the results
+list(
+  before_pandemic = list(
+    solar65a = total_before_solar65a,
+    solar75a = total_before_solar75a,
+    solar83a = total_before_solar83a
+  ),
+  after_pandemic = list(
+    solar65a = total_after_solar65a,
+    solar75a = total_after_solar75a,
+    solar83a = total_after_solar83a
+  )
+)
